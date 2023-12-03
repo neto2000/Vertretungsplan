224f030b1c6b43a48d75bd1f2e02985ef77b3b87
neto2000 <82058954+neto2000@users.noreply.github.com>
2023-12-03 23:02:43 +0100

    added Info und Table.svelte

---
+++ frontend/src/lib/Table.svelte
@@ -0,0 +1,25 @@
+<script>
+
+</script>
+
+
+<div class="table">
+  <table>
+    <tr>
+      <th>Klasse</th>
+      <th>Stunde</th>
+      <th>altes Fach</th>
+      <th>abwesend</th>
+      <th>Vertreter</th>
+      <th>neues Fach</th>
+      <th>Raum</th>
+      <th>Typ</th>
+      <th>Info</th>
+    </tr>
+  </table>
+</div>
+
+
+<style>
+
+</style>
