4f923ae3badd4bd2360e0199ca75b89a5534e566
neto2000 <82058954+neto2000@users.noreply.github.com>
2024-01-02 19:10:18 +0100

    added todo

--- frontend/src/lib/Login.svelte
+++ frontend/src/lib/Login.svelte
@@ -3,11 +3,30 @@
 </script>
 
 
-<div class="login-container">
+<div class="login-site">
+  <div class="login-container">
 
+
+  </div>
 </div>
 
 
 <style>
+  .login-site{
 
+    display: flex;
+
+    justify-content: center;
+
+    align-content: center;
+  }
+
+  .login-container {
+
+    width: 40%;
+
+    height: 60vh;
+
+    background-color: black;
+  } 
 </style>
