6db8e3afeee1b4a430ec13db196c9c9acb7ffa8d
neto2000 <82058954+neto2000@users.noreply.github.com>
2024-01-16 21:45:59 +0100

    changed Admin_Stundenplan to Admin_plan

---
+++ frontend/src/lib/Admin_Plan.svelte
@@ -0,0 +1,36 @@
+<script>
+
+</script>
+
+
+<section>
+  <div class="plan-container">
+    <h2 class="plan-heading">Montag der 08.09.2023</h2>
+
+    <div class="button-container">
+      <button class="active">Heute</button>
+      <button class="not-active">Morgen</button>
+    </div>
+
+    <div class="info-container">
+      <h3 class="info-head">Infos</h3>
+
+      <div class="infos">
+        {#each Array(4) as _, i}
+          <Info text={"Das ist eine wichtige Info!"} />
+        {/each}
+      </div>
+
+    </div>
+
+    <Table />
+
+
+  </div>
+
+</section>
+
+
+<style>
+
+</style>
