5a2306ce15007bb0e810f24a2c162d38229edd63
neto2000 <82058954+neto2000@users.noreply.github.com>
2024-02-02 23:12:46 +0100

    completed plan sql migration

--- backend/migrations/0003_plan.sql
+++ backend/migrations/0003_plan.sql
@@ -2,4 +2,15 @@ CREATE TABLE plan (
 	id int not null AUTO_INCREMENT,
 	day int not null,
 	class varchar(255) not null,
+	start_hour int not null,
+	end_hour int not null,
+	old_fach varchar(255) not null,
+	new_fach varchar(255),
+	away varchar(255),
+	sub varchar(255),
+	room varchar(255),
+	typ varchar(255),
+	info varchar(255),
+	PRIMARY KEY(id),
+	FOREIGN KEY(day) REFERENCES day(id)
 );
