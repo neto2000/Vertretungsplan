2f5a631aebaca13649fe12da14e618d11effc094
neto2000 <82058954+neto2000@users.noreply.github.com>
2024-07-09 21:56:31 +0200

    admin: rows update automatically

--- frontend/src/lib/Admin_Plan_Row.svelte
+++ frontend/src/lib/Admin_Plan_Row.svelte
@@ -1,5 +1,7 @@
 <script>
 
+  import { get } from "svelte/store";
+
   import { admin_rows, changed_rows } from "../stores";
 
   export let id;
@@ -35,32 +37,64 @@
     changed_rows.set([...changed, row_list]);
   }
 
+  async function update_changed_row() {
+
+    console.log("row changed with on:changed")
+
+    console.log(get(changed_rows))
+
+    for (let i = 0; i < changed.length; i++) {
+
+      if (changed[i].id == row_list.id) {
+
+        return; 
+      }
+    }
+
+    changed_rows.set([...changed, row_list]);
+
+
+    // send whole changed list!
+    const res = await fetch('/update', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(get(changed_rows))
+    })
+
+    console.log("updated")
+
+    changed_rows.set([]);
+
+  }
+
 
 </script>
 
 <tr class="row">
 
-  <td><input type="text" bind:value={row_list.class} on:input={row_to_changed_list} class="input-field" style="width: 90%;"></td>
+  <td><input type="text" bind:value={row_list.class} on:change={update_changed_row} class="input-field" style="width: 90%;"></td>
 
   <td style='white-space: nowrap'>
-    <input type="number" bind:value={row_list.start_hour} on:input={row_to_changed_list} class="input-field" style="width: 3vw; display: inline-block;">
+    <input type="number" bind:value={row_list.start_hour} on:change={update_changed_row} class="input-field" style="width: 3vw; display: inline-block;">
 
-    <input type="number" bind:value={row_list.end_hour} on:input={row_to_changed_list} class="input-field" style="width: 3vw; display: inline-block;">
+    <input type="number" bind:value={row_list.end_hour} on:change={update_changed_row} class="input-field" style="width: 3vw; display: inline-block;">
   </td>
 
-  <td><input type="text" bind:value={row_list.old_fach} on:input={row_to_changed_list} class="input-field" style="width: 90%;"></td>
+  <td><input type="text" bind:value={row_list.old_fach} on:change={update_changed_row} class="input-field" style="width: 90%;"></td>
 
-  <td><input type="text" bind:value={row_list.away} on:input={row_to_changed_list} class="input-field" style="width: 90%;"></td>
+  <td><input type="text" bind:value={row_list.away} on:change={update_changed_row} class="input-field" style="width: 90%;"></td>
 
-  <td><input type="text" bind:value={row_list.sub} on:input={row_to_changed_list} class="input-field" style="width: 90%;"></td>
+  <td><input type="text" bind:value={row_list.sub} on:change={update_changed_row} class="input-field" style="width: 90%;"></td>
 
-  <td><input type="text" bind:value={row_list.new_fach} on:input={row_to_changed_list} class="input-field" style="width: 90%;"></td>
+  <td><input type="text" bind:value={row_list.new_fach} on:change={update_changed_row} class="input-field" style="width: 90%;"></td>
 
-  <td><input type="text" bind:value={row_list.room} on:input={row_to_changed_list} class="input-field" style="width: 90%;"></td>
+  <td><input type="text" bind:value={row_list.room} on:change={update_changed_row} class="input-field" style="width: 90%;"></td>
 
-  <td><input type="text" bind:value={row_list.typ} on:input={row_to_changed_list} class="input-field" style="width: 90%;"></td>
+  <td><input type="text" bind:value={row_list.typ} on:change={update_changed_row} class="input-field" style="width: 90%;"></td>
 
-  <td><input type="text" bind:value={row_list.info} on:input={row_to_changed_list} class="input-field" style="width: 90%;"></td>
+  <td><input type="text" bind:value={row_list.info} on:change={update_changed_row} class="input-field" style="width: 90%;"></td>
 
 </tr>
 
